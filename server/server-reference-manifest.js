0e8af003cbed0da249978f2128adab9beff23f8b
Poowadol Rungtabnapa <Poowadol.dev@gmail.com>
2024-07-07 20:21:20 +0700

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{\"b81e3363685129422fafde2a6728cc8adef10015\":{\"workers\":{\"app/project/page\":5929},\"layer\":{\"app/project/page\":\"rsc\"}}},\"edge\":{},\"encryptionKey\":\"WqI+Wff4Wto1HBA7b60fT14sr7utZ2CcVjIJnQBYz7I=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{\"b81e3363685129422fafde2a6728cc8adef10015\":{\"workers\":{\"app/project/page\":5929},\"layer\":{\"app/project/page\":\"rsc\"}}},\"edge\":{},\"encryptionKey\":\"bKVh1DIyXmg1nsapTUoDr+mou5NeDYBp90yBIHrDrNA=\"}"
